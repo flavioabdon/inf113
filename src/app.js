fd988d44666152afd6fa5ded21813338f7a33055
flaviocondori <flaviocondori@gmail.com>
2018-06-01 11:35:21 -0400

    fix app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
+const express = require('express'),
+      path = require('path'),
+      morgan = require('morgan'),
+      mysql = require('mysql'),
+      myConnection = require('express-myconnection');
 
+const app = express();
+
+// importing routes
+const customerRoutes = require('./routes/customer');
+
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// middlewares
 app.use(myConnection(mysql, {
   host: 'localhost',
   user: 'root',
   password: '123',
   port: 3306,
-  //base de datos alquileres
   database: 'alquileres'
 }, 'single'));
 app.use(express.urlencoded({extended: false}));
