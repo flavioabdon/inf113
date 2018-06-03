8971373943dffa1e463f624dc7c376d5ab37baf4
flaviocondori <flaviocondori@gmail.com>
2018-06-03 09:19:54 -0400

    header, nav

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -2,12 +2,6 @@ const router = require('express').Router();
 
 const customerController = require('../controllers/customerController');
 
-router.get('/', customerController.list);
-router.post('/add', customerController.save);
-router.get('/update/:id', customerController.edit);
-router.post('/update/:id', customerController.update);
-router.get('/delete/:id', customerController.delete);
-
 router.get('/ambiente/', customerController.listarAmb);
 router.get('/contrato/', customerController.listarCon);
 router.get('/pago/', customerController.listarPag);
